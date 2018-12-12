293650a6f1078154917c4665c2baebc60ee741c7
ItaloFaud <italofaud10@hotmail.com>
2018-12-12 20:57:41 -0200

    Finalizando pacote Admin

--- src/View/Admin/Procura_Funcionario.java
+++ src/View/Admin/Procura_Funcionario.java
@@ -165,7 +165,7 @@ public class Procura_Funcionario extends javax.swing.JFrame {
     
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        new Controle_Escritório().setVisible(true);
+        new AdministracaoGeral().setVisible(true);
         dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
